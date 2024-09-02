76afeea4ff179b73be2b84b5215b375eac254198
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-09-02 22:07:42 +0200

    Create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db').sequelize; // Ensure correct path and export
+
+// Define the User model
+const User = sequelize.define(
+    'User',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = User;
