9602a83b5fd6f570d11ccae5d3e55215e10958bf
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-09-05 21:33:15 +0200

    Create middleware for field validation

---
+++ server/middlewares/validateUserFields.js
@@ -0,0 +1,33 @@
+const { body } = require('express-validator');
+
+const validateUserFields = [
+    body('firstName')
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('First name is required'),
+    body('lastName')
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('Last name is required'),
+    body('username')
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('Username is required'),
+    body('email')
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Valid email is required'),
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8, max: 12 })
+        .withMessage('Password must be between 8 and 12 characters long'),
+];
+
+module.exports = validateUserFields;
