caf7964f870907b0b3f2c60f97765465eb33afd4
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-09-05 21:34:43 +0200

    Add middleware to post route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const validateUserFields = require('../middlewares/validateUserFields');
 
 // Additional user routes
 router.get('/check-availability', userController.checkAvailability);
@@ -10,7 +11,7 @@ router.get('/', userController.getUsers);
 
 router.get('/:id', userController.getUserByID);
 
-router.post('/', userController.createUser);
+router.post('/', validateUserFields, userController.createUser);
 
 router.put('/:id', userController.updateUser);
 
