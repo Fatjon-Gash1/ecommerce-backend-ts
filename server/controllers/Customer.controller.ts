3416c0d891b2f959be4fa7f9b81740876dd76094
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-09-09 22:38:29 +0200

    Add model query

--- server/controllers/Customer.controller.ts
+++ server/controllers/Customer.controller.ts
@@ -1,133 +1,216 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+import { User, Customer } from '../models/relational';
 
-const User = require('../models/user.model');
-const bcrypt = require('bcrypt');
-const { validationResult } = require('express-validator');
-
-const getUsers = async (req, res) => {
+// Get all customers
+export const getCustomers = async (
+    _req: Request,
+    res: Response
+): Promise<void> => {
     try {
-        const users = await User.findAll();
-        res.status(200).json({ users });
+        const customers = await Customer.findAll({
+            include: {
+                model: User,
+                as: 'user',
+                attributes: ['firstName', 'lastName', 'username', 'email'],
+            },
+        });
+
+        res.status(200).json({ customers });
     } catch (err) {
-        console.log('Error getting users:', err);
+        console.error('Error getting customers:', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
-const getUserByID = async (req, res) => {
-    const id = req.params.id;
+// Get customer by ID
+export const getCustomerByID = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const id = parseInt(req.params.id, 10);
     try {
-        const user = await User.findByPk(id);
-        res.status(200).json({ user });
+        const customer = await Customer.findByPk(id, {
+            include: {
+                model: User,
+                as: 'user',
+                attributes: ['firstName', 'lastName', 'username', 'email'],
+            },
+        });
+
+        if (!customer) {
+            res.status(404).json({ message: 'Customer not found' });
+            return;
+        }
+        res.status(200).json({ customer });
     } catch (error) {
-        console.log('Error fetching user:', error);
+        console.error('Error fetching customer:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 };
 
-const createUser = async (req, res) => {
+// Create new customer
+export const createCustomer = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        res.status(400).json({ errors: errors.array() });
+        return;
     }
 
     try {
-        const { firstName, lastName, username, email, password } = req.body;
+        const {
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+            shippingAddress,
+            billingAddress,
+        } = req.body;
 
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        const newUser = await User.create({
+        const newCustomer = await Customer.create({
             firstName,
             lastName,
             username,
             email,
             password: hashedPassword,
+            shippingAddress,
+            billingAddress,
         });
 
-        res.status(201).json({ newUser });
+        res.status(201).json({ newCustomer });
     } catch (err) {
-        console.error('Error creating user: ', err);
+        console.error('Error creating customer: ', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
-const updateUser = async (req, res) => {
+// Update customer
+export const updateCustomer = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+
     try {
-        const id = req.params.id;
-        const { firstName, lastName, username, email, password } = req.body;
-        const updated = await User.update(
-            { firstName, lastName, username, email, password },
+        const id = parseInt(req.params.id, 10);
+        const {
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+            shippingAddress,
+            billingAddress,
+        } = req.body;
+
+        const saltRounds = 10;
+        const hashedPassword: string = await bcrypt.hash(password, saltRounds);
+
+        const updated = await Customer.update(
+            {
+                firstName,
+                lastName,
+                username,
+                email,
+                password: hashedPassword,
+                shippingAddress,
+                billingAddress,
+            },
             { where: { id }, returning: true }
         );
 
         if (updated) {
-            const updatedUser = await User.findByPk(id);
-            res.json({ updatedUser });
+            const updatedCustomer = await Customer.findByPk(id);
+            res.json({ updatedCustomer });
         } else {
-            res.status(404).json({ error: 'User not found' });
+            res.status(404).json({ error: 'Customer not found' });
         }
     } catch (err) {
-        console.error('Error updating user: ', err);
+        console.error('Error updating customer: ', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
-const deleteUser = async (req, res) => {
+// Delete customer
+export const deleteCustomer = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
     try {
-        const id = req.params.id;
-        const deleted = await User.destroy({ where: { id } });
-
-        if (deleted) {
-            res.status(204).send();
-        } else {
-            res.status(404).json({ error: 'User not found' });
+        const id = parseInt(req.params.id, 10);
+        const customer = await Customer.findByPk(id);
+        if (!customer) {
+            res.status(404).json({ message: 'Customer not found' });
+            return;
         }
+
+        // Find the associated user
+        const user = await User.findByPk(customer.userId);
+
+        // Delete the customer
+        await customer.destroy();
+
+        // Soft delete the associated user
+        if (user) {
+            await user.destroy(); // This triggers the soft delete and sets `deletedAt`
+        }
+
+        res.status(200).json({
+            message:
+                'Customer and associated user deleted (soft delete for user)',
+        });
     } catch (err) {
-        console.error('Error deleting user: ', err);
+        console.error('Error deleting customer: ', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
-const checkAvailability = async (req, res) => {
+// Check availability of username or email
+export const checkAvailability = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
     const { username, email } = req.query;
 
-    // Determine the field to check
     const field = username ? 'username' : email ? 'email' : null;
 
     if (!field) {
-        return res
-            .status(400)
-            .json({ message: 'Username or Email is required' });
+        res.status(400).json({ message: 'Username or Email is required' });
+        return;
     }
 
     try {
-        const userExists = await User.findOne({
+        const customerExists = await Customer.findOne({
             where: { [field]: req.query[field] },
         });
 
-        if (userExists) {
-            return res.status(200).json({
+        if (customerExists) {
+            res.status(200).json({
                 available: false,
                 message: `${field.charAt(0).toUpperCase() + field.slice(1)} is taken`,
             });
         } else {
-            return res.status(200).json({
+            res.status(200).json({
                 available: true,
                 message: `${field.charAt(0).toUpperCase() + field.slice(1)} is available`,
             });
         }
     } catch (error) {
         console.error(`Error checking ${field} availability:`, error);
-        return res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Server error' });
     }
-};
-
-module.exports = {
-    getUsers,
-    getUserByID,
-    createUser,
-    updateUser,
-    deleteUser,
-    checkAvailability,
 };
