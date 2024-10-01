cdcc7d9e0fe170bf1e9d5f17ef7fb14cb44e511d
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 00:47:57 +0200

    Create product analytics model

---
+++ server/models/document/ProductAnalytic.model.ts
@@ -0,0 +1,25 @@
+import { mongoose } from '../../config/mongodb';
+const { Schema, model } = mongoose;
+
+interface IProductAnalytic extends Document {
+    productId: number;
+    purchases: number;
+    reviews: number;
+    averageRating: number;
+    date?: Date;
+}
+
+const productAnalyticSchema = new Schema<IProductAnalytic>({
+    productId: { type: Number, required: true },
+    purchases: { type: Number, default: 0 },
+    reviews: { type: Number, default: 0 },
+    averageRating: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now },
+});
+
+const ProductAnalytic = model<IProductAnalytic>(
+    'ProductAnalytic',
+    productAnalyticSchema
+);
+
+export default ProductAnalytic;
