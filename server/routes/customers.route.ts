733368a1c4acf939baed9e14b64a217e57d74b58
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 01:03:57 +0200

    Alternate route

--- server/routes/customers.route.ts
+++ server/routes/customers.route.ts
@@ -1,25 +1,18 @@
 import { Router, Request, Response } from 'express';
-import {
-    checkAvailability,
-    getCustomers,
-    getCustomerByID,
-    createCustomer,
-    updateCustomer,
-    deleteCustomer,
-} from '../controllers/Customer.controller';
+import { getCustomers } from '../controllers/Customer.controller';
 import validateUserFields from '../middlewares/validateUserFields';
 
 const router: Router = Router();
 
 // Additional customer routes
-router.get('/check-availability', (req: Request, res: Response) =>
+/*router.get('/check-availability', (req: Request, res: Response) =>
     checkAvailability(req, res)
-);
+);*/
 
 // Define CRUD customer routes
 router.get('/', (req: Request, res: Response) => getCustomers(req, res));
 
-router.get('/:id', (req: Request, res: Response) => getCustomerByID(req, res));
+/*router.get('/:id', (req: Request, res: Response) => getCustomerByID(req, res));
 
 router.post('/', validateUserFields, (req: Request, res: Response) =>
     createCustomer(req, res)
@@ -31,6 +24,6 @@ router.put('/:id', validateUserFields, (req: Request, res: Response) =>
 
 router.delete('/:id', (req: Request, res: Response) =>
     deleteCustomer(req, res)
-);
+);*/
 
 export default router;
