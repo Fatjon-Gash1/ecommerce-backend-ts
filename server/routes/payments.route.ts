2e7fd20b022b155de662faa989a9bbd14fe204da
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 00:56:24 +0200

    Create payments route

---
+++ server/routes/payments.route.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { PaymentController } from '../controllers/Payment.controller';
+import { PaymentService } from '../services/Payment.service';
+
+const router: Router = Router();
+const paymentService = new PaymentService(process.env.STRIPE_KEY as string);
+
+const paymentController = new PaymentController(paymentService);
+
+router.get(
+    '/payment-intent/:id',
+    paymentController.getPaymentIntent.bind(paymentController)
+);
+
+router.post(
+    '/payment-intent',
+    paymentController.createPaymentIntent.bind(paymentController)
+);
+
+export default router;
