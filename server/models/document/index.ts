da626e6bbb7fdf2c372b0510412a06c0c79c48a3
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 00:49:13 +0200

    Add to models to index

--- server/models/document/index.ts
+++ server/models/document/index.ts
@@ -1,3 +1,14 @@
-import Rating from './PlatformRating.model';
+import Rating, { IRating } from './Rating.model';
+import PlatformRating, { IPlatformRating } from './PlatformRating.model';
+import ProductReview, { IProductReview } from './ProductReview.model';
+import ProductAnalytic from './ProductAnalytic.model';
 
-export { Rating };
+export {
+    Rating,
+    IRating,
+    PlatformRating,
+    IPlatformRating,
+    ProductReview,
+    IProductReview,
+    ProductAnalytic,
+};
