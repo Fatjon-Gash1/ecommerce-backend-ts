114ce2599f1b75543259b36c8c3a11e313d4c4d3
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 00:39:02 +0200

    Add new env vars

--- server/env.d.ts
+++ server/env.d.ts
@@ -7,5 +7,12 @@ declare namespace NodeJS {
         MYSQL_PASSWORD?: string;
         MYSQL_DATABASE?: string;
         MONGODB_URI?: string;
+        ACCESS_TOKEN_KEY?: string;
+        REFRESH_TOKEN_KEY?: string;
+        ACCESS_TOKEN_EXPIRY?: string;
+        REFRESH_TOKEN_EXPIRY?: string;
+	GMAIL_USER?: string;
+	GMAIL_PASS?: string;
+        STRIPE_KEY?: string;
     }
 }
