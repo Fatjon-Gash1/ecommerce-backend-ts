850ce0754d0f33232492b7d1241373503d8594a8
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 01:07:36 +0200

    Add reducers to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
 import productReducer from './features/product/productSlice';
+import platformReducer from './features/platform/platformSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         product: productReducer,
+        platform: platformReducer,
     },
 });
 
