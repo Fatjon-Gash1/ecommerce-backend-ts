b31961fe4e70c120b7dbe3be5b6da6844ec7812c
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-02 00:47:33 +0200

    Clean model definition

--- server/models/document/PlatformRating.model.ts
+++ server/models/document/PlatformRating.model.ts
@@ -1,28 +1,22 @@
-import { mongoose } from '../../config/db';
-const { Schema, model } = mongoose;
+import { mongoose } from '../../config/mongodb';
+const { Schema } = mongoose;
+import Rating, { IRating } from './Rating.model';
 
-interface IRating extends Document {
-    userId: number;
-    username: string;
-    firstName: string;
-    lastName: string;
-    userProfession?: string;
-    rating: number;
-    review: string;
-    date?: Date;
+export interface IPlatformRating extends IRating {
+    numberOfPurchases?: number;
+    mostPurchasesCategory?: string;
+    featureHighlights: string;
 }
 
-const ratingSchema = new Schema<IRating>({
-    userId: { type: Number, required: true },
-    username: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    userProfession: { type: String, required: false },
-    rating: { type: Number, required: true, min: 1, max: 5 },
-    review: { type: String, required: true },
-    date: { type: Date, default: Date.now },
+const platformRatingSchema = new Schema<IPlatformRating>({
+    numberOfPurchases: { type: Number, default: 0, required: false },
+    mostPurchasesCategory: { type: String, default: '', required: false },
+    featureHighlights: { type: String, required: false },
 });
 
-const Rating = model<IRating>('Rating', ratingSchema);
+const PlatformRating = Rating.discriminator<IPlatformRating>(
+    'PlatformRating',
+    platformRatingSchema
+);
 
-export default Rating;
+export default PlatformRating;
