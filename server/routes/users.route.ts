b147b7dd62cd2e4387e803e444513b5f7f8fe6b4
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-06 23:15:14 +0200

    Redefine users route

--- server/routes/users.route.ts
+++ server/routes/users.route.ts
@@ -1,71 +1,62 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/User.controller';
-import {
-    UserService,
-    NotificationService,
-    AdminLogsService,
-} from '../services';
+import { UserService, NotificationService } from '../services';
+import authenticateRefreshToken from '../middlewares/authentication/refreshToken';
+import authenticateAccessToken from '../middlewares/authentication/accessToken';
+import authorize from '../middlewares/authorization/authorize';
+import adminRoutes from './admins.route';
+
+const router: Router = Router();
 const userService = new UserService();
 const notificationService = new NotificationService();
-const adminLogsService = new AdminLogsService();
-const userController = new UserController(
-    userService,
-    notificationService,
-    adminLogsService
-);
-const router: Router = Router();
+const userController = new UserController(userService, notificationService);
 
 router.post('/auth/signup', userController.signUpUser.bind(userController));
 router.post('/auth/login', userController.loginUser.bind(userController));
-
 router.post(
-    '/admin/customers',
-    userController.registerCustomer.bind(userController)
+    '/auth/tokens',
+    authenticateRefreshToken,
+    userController.generateTokens.bind(userController)
 );
-router.post('/admin/admins', userController.registerAdmin.bind(userController));
+router.post(
+    '/auth/logout',
+    authenticateAccessToken,
+    userController.logoutUser.bind(userController)
+);
 
 router.get(
     '/availability',
     userController.checkUserAvailability.bind(userController)
 );
-
 router.get(
-    '/customers/:id',
-    userController.getCustomerById.bind(userController)
+    '/customers/profile',
+    authenticateAccessToken,
+    userController.getCustomerProfile.bind(userController)
 );
-router.get(
-    '/customers/active',
-    userController.findActiveCustomers.bind(userController)
-);
-router.get(
-    '/customers/search',
-    userController.findCustomerByAttribute.bind(userController)
-);
-router.get('/customers', userController.getAllCustomers.bind(userController));
-
-router.get('/admins/:id', userController.getAdminById.bind(userController));
-router.get(
-    '/admins/search',
-    userController.getAdminsByRole.bind(userController)
-);
-router.get('/admins', userController.getAllAdmins.bind(userController));
 
 router.patch(
-    '/:id/password',
+    '/password',
+    authenticateAccessToken,
     userController.changePassword.bind(userController)
 );
-router.put('/:id/', userController.updateUser.bind(userController));
-
 router.patch(
-    '/customers/:id/shipping-details',
-    userController.updateShippingDetails.bind(userController)
+    '/customers/customer-details',
+    authenticateAccessToken,
+    authorize('customer'),
+    userController.updateCustomerDetails.bind(userController)
+);
+router.patch(
+    '/',
+    authenticateAccessToken,
+    userController.updateUser.bind(userController)
 );
 
-router.patch(
-    '/admins/:id/role',
-    userController.setAdminRole.bind(userController)
+router.delete(
+    '/',
+    authenticateAccessToken,
+    userController.deleteUser.bind(userController)
 );
 
-router.delete('/:id', userController.deleteUser.bind(userController));
+router.use('/admin', authenticateAccessToken, authorize('admin'), adminRoutes);
 
 export default router;
