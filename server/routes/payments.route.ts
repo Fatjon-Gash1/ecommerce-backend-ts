69906aedfd45689821a82e0495f7c7fa3aa222a9
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-06 23:06:18 +0200

    Generalize service import

--- server/routes/payments.route.ts
+++ server/routes/payments.route.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { PaymentController } from '../controllers/Payment.controller';
-import { PaymentService } from '../services/Payment.service';
+import { PaymentService } from '../services';
 
 const router: Router = Router();
 const paymentService = new PaymentService(process.env.STRIPE_KEY as string);
-
 const paymentController = new PaymentController(paymentService);
 
 router.get(
