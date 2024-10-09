28cd6c29adf1cbb81bc45df34e7b381460a4b9d0
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-09 21:59:15 +0200

    Change param to array

--- server/middlewares/authorization/authorize.ts
+++ server/middlewares/authorization/authorize.ts
@@ -1,19 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { JwtPayload } from 'jsonwebtoken';
 
-export default function authorize(requiredRole: string) {
-    return (req: Request, res: Response, next: NextFunction) => {
+export default function authorize(providedRoles: string[]) {
+    return (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): void | Response => {
         if (req.user && typeof req.user !== 'string') {
             const userRole = (req.user as JwtPayload).role;
 
-            if (userRole !== requiredRole) {
-                res.status(403).json({ message: 'Access denied' });
-                return;
+            if (!providedRoles.includes(userRole)) {
+                return res.status(403).json({ message: 'Access denied' });
             }
 
             next();
         } else {
-            res.status(400).json({ message: 'Invalid decoded data' });
+            return res.status(400).json({ message: 'Invalid decoded data' });
         }
     };
 }
