81e0c253b191d4bbdaec18c18348d929a8b1e90c
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-11 22:25:49 +0200

    Create & move old files to protected routes dir

---
+++ server/routes/protected/private/admins.route.ts
@@ -0,0 +1,104 @@
+import { Router } from 'express';
+import { AdminController } from '../../../controllers/Admin.controller';
+import {
+    AdminService,
+    NotificationService,
+    AdminLogsService,
+} from '../../../services';
+import authorize from '../../../middlewares/authorization/authorize';
+import {
+    registerRateLimiter,
+    userDeletionRateLimiter,
+} from '../../../middlewares/rateLimiting';
+import {
+    validateRegistration,
+    validateId,
+    validateAttribute,
+    validateAdminRole,
+    validateAdminRoleSet,
+    validationErrors,
+} from '../../../middlewares/validation';
+import adminProducts from './adminProducts.route';
+
+const router: Router = Router();
+const adminController = new AdminController(
+    new AdminService(),
+    new NotificationService(),
+    new AdminLogsService()
+);
+
+router.post(
+    '/customers',
+    authorize(['admin']),
+    registerRateLimiter,
+    validateRegistration(),
+    validationErrors,
+    adminController.registerCustomer.bind(adminController)
+);
+router.post(
+    '/admins',
+    authorize(['admin']),
+    registerRateLimiter,
+    validateRegistration(),
+    validationErrors,
+    adminController.registerAdmin.bind(adminController)
+);
+
+router.get(
+    '/customers/:id',
+    validateId(),
+    validationErrors,
+    adminController.getCustomerById.bind(adminController)
+);
+router.get(
+    '/customers/active',
+    adminController.findActiveCustomers.bind(adminController)
+);
+router.get(
+    '/customers/search',
+    validateAttribute(),
+    validationErrors,
+    adminController.findCustomerByAttribute.bind(adminController)
+);
+router.get('/customers', adminController.getAllCustomers.bind(adminController));
+
+router.get(
+    '/admins/:id',
+    authorize(['admin']),
+    validateId(),
+    validationErrors,
+    adminController.getAdminById.bind(adminController)
+);
+router.get(
+    '/admins/search',
+    authorize(['admin']),
+    validateAdminRole(),
+    validationErrors,
+    adminController.getAdminsByRole.bind(adminController)
+);
+router.get(
+    '/admins',
+    authorize(['admin']),
+    adminController.getAllAdmins.bind(adminController)
+);
+
+router.patch(
+    '/admins/:id/role',
+    authorize(['admin']),
+    validateAdminRoleSet(),
+    validationErrors,
+    adminController.setAdminRole.bind(adminController)
+);
+
+router.delete(
+    '/admins/:id',
+    authorize(['admin']),
+    userDeletionRateLimiter,
+    validateId(),
+    validationErrors,
+    adminController.deleteUserById.bind(adminController)
+);
+
+router.use('/products', adminProducts);
+
+export default router;
