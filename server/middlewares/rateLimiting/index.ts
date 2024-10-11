3101b12fa437e7d89369838cb2e62c3829816972
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-11 22:27:59 +0200

    Export admin product rate limiters

--- server/middlewares/rateLimiting/index.ts
+++ server/middlewares/rateLimiting/index.ts
@@ -5,3 +5,17 @@ export {
     updateRateLimiter,
     passwordChangeRateLimiter,
 } from './userRateLimiters';
+
+export {
+    registerRateLimiter,
+    userDeletionRateLimiter,
+} from './admin/adminUserRateLimiters';
+
+export {
+    categoryCreationRateLimiter,
+    productCreationRateLimiter,
+    categoryUpdateRateLimiter,
+    productUpdateRateLimiter,
+    categoryDeletionRateLimiter,
+    productDeletionRateLimiter,
+} from './admin/adminProductRateLimiters';
