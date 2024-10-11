11542d0726e9a9a31a2b4be1cb6fd670a74c240c
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-11 22:21:12 +0200

    Remove unnecessary validation middleware

--- server/middlewares/validation/adminValidations.ts
+++ server/middlewares/validation/adminValidations.ts
@@ -19,17 +19,6 @@ export const validateAdminRoleSet = (): ValidationChain[] => [
         .withMessage('Invalid role number'),
 ];
 
-export const validateAdminUsername = (): ValidationChain[] => [
-    body('username')
-        .trim()
-        .notEmpty()
-        .withMessage('Username is required')
-        .matches(/^[a-zA-Z0-9._-]+$/)
-        .withMessage(
-            'Username must contain only letters, numbers, underscores, dots, or hyphens'
-        ),
-];
-
 export const validateAttribute = (): ValidationChain[] => [
     query('attribute')
         .trim()
@@ -44,12 +33,4 @@ export const validateAttribute = (): ValidationChain[] => [
         .withMessage('Value is required')
         .isLength({ max: 32 })
         .withMessage('Value must be 32 characters or less'),
-];
-
-export const validateId = (): ValidationChain[] => [
-    param('id')
-        .notEmpty()
-        .withMessage('Id is required')
-        .isInt({ min: 1 })
-        .withMessage('Id must be a positive integer'),
 ];
