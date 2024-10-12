5d15342f1e7070e6c1701df2f5b33b9faa621ef1
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-12 21:45:44 +0200

    Change method's req input property & reorder methods

--- server/controllers/Rating.controller.ts
+++ server/controllers/Rating.controller.ts
@@ -5,6 +5,7 @@ import {
     ProductNotFoundError,
     RatingNotFoundError,
 } from '../errors';
+import { JwtPayload } from 'jsonwebtoken';
 
 export class RatingController {
     private ratingService: RatingService;
@@ -41,6 +42,31 @@ export class RatingController {
         }
     }
 
+    public async addProductRating(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const { details } = req.body;
+
+        try {
+            const rating = await this.ratingService.addProductRating(details);
+            return res.status(201).json({ rating });
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                console.error('Error adding rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            if (error instanceof ProductNotFoundError) {
+                console.error('Error adding rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error adding rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
     public async getPlatformRatings(
         _req: Request,
         res: Response
@@ -54,14 +80,15 @@ export class RatingController {
         }
     }
 
-    public async getPlatformRating(
+    public async getPlatformRatingById(
         req: Request,
         res: Response
     ): Promise<void | Response> {
         const ratingId: number = Number(req.params.id);
 
         try {
-            const rating = await this.ratingService.getPlatformRating(ratingId);
+            const rating =
+                await this.ratingService.getPlatformRatingById(ratingId);
             return res.status(200).json({ rating });
         } catch (error) {
             if (error instanceof RatingNotFoundError) {
@@ -95,11 +122,74 @@ export class RatingController {
         }
     }
 
-    public async updatePlatformRating(
+    public async getProductRatings(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const productId: number = Number(req.params.id);
+
+        try {
+            const productRatings =
+                await this.ratingService.getProductRatings(productId);
+            return res.status(200).json({ productRatings });
+        } catch (error) {
+            if (error instanceof ProductNotFoundError) {
+                console.error('Error getting rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error getting rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async getProductRatingById(
         req: Request,
         res: Response
     ): Promise<void | Response> {
         const ratingId: number = Number(req.params.id);
+
+        try {
+            const productRating =
+                await this.ratingService.getProductRatingById(ratingId);
+            return res.status(200).json({ productRating });
+        } catch (error) {
+            if (error instanceof RatingNotFoundError) {
+                console.error('Error getting rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error getting rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async getProductRatingsByCustomer(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const userId: number = Number(req.params.id);
+
+        try {
+            const productRating =
+                await this.ratingService.getProductRatingsByCustomer(userId);
+            return res.status(200).json({ productRating });
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                console.error('Error getting rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error getting rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async updateOwnPlatformRating(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const ratingId: number = Number((req.user as JwtPayload).id);
         const { details } = req.body;
 
         try {
@@ -119,20 +209,39 @@ export class RatingController {
         }
     }
 
-    public async deletePlatformRating(
+    public async updateOwnProductRating(
         req: Request,
         res: Response
     ): Promise<void | Response> {
-        const ratingId: number = Number(req.params.id);
-        const { username } = req.body;
+        const ratingId: number = Number((req.user as JwtPayload).id);
+        const { details } = req.body;
 
         try {
-            await this.ratingService.deletePlatformRating(ratingId);
-            res.sendStatus(204);
-
-            if (username) {
-                await this.adminLogsService!.log(username, 'rating', 'delete');
+            const updatedRating = await this.ratingService.updateProductRating(
+                ratingId,
+                details
+            );
+            return res.status(200).json({ updatedRating });
+        } catch (error) {
+            if (error instanceof RatingNotFoundError) {
+                console.error('Error updating rating: ', error);
+                return res.status(404).json({ message: error.message });
             }
+
+            console.error('Error updating rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async deleteOwnPlatformRating(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const ratingId: number = Number((req.user as JwtPayload).id);
+
+        try {
+            await this.ratingService.deletePlatformRatingById(ratingId);
+            res.sendStatus(204);
         } catch (error) {
             if (error instanceof RatingNotFoundError) {
                 console.error('Error deleting rating: ', error);
@@ -144,139 +253,68 @@ export class RatingController {
         }
     }
 
-    public async addProductReview(
+    public async deletePlatformRatingById(
         req: Request,
         res: Response
     ): Promise<void | Response> {
-        const { details } = req.body;
+        const ratingId: number = Number(req.params.id);
+        const { username } = req.user as JwtPayload;
 
         try {
-            const review = await this.ratingService.addProductReview(details);
-            return res.status(201).json({ review });
-        } catch (error) {
-            if (error instanceof UserNotFoundError) {
-                console.error('Error adding review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            if (error instanceof ProductNotFoundError) {
-                console.error('Error adding review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            console.error('Error adding review: ', error);
-            return res.status(500).json({ message: 'Server error' });
-        }
-    }
-
-    public async getProductReviews(
-        req: Request,
-        res: Response
-    ): Promise<void | Response> {
-        const productId: number = Number(req.params.id);
-
-        try {
-            const productReviews =
-                await this.ratingService.getProductReviews(productId);
-            return res.status(200).json({ productReviews });
-        } catch (error) {
-            if (error instanceof ProductNotFoundError) {
-                console.error('Error getting review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            console.error('Error getting review: ', error);
-            return res.status(500).json({ message: 'Server error' });
-        }
-    }
-
-    public async getProductReview(
-        req: Request,
-        res: Response
-    ): Promise<void | Response> {
-        const reviewId: number = Number(req.params.id);
-
-        try {
-            const productReview =
-                await this.ratingService.getProductReview(reviewId);
-            return res.status(200).json({ productReview });
-        } catch (error) {
-            if (error instanceof RatingNotFoundError) {
-                console.error('Error getting review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            console.error('Error getting review: ', error);
-            return res.status(500).json({ message: 'Server error' });
-        }
-    }
-
-    public async getProductReviewsByCustomer(
-        req: Request,
-        res: Response
-    ): Promise<void | Response> {
-        const userId: number = Number(req.params.id);
-
-        try {
-            const productReview =
-                await this.ratingService.getProductReviewsByCustomer(userId);
-            return res.status(200).json({ productReview });
-        } catch (error) {
-            if (error instanceof UserNotFoundError) {
-                console.error('Error getting review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            console.error('Error getting review: ', error);
-            return res.status(500).json({ message: 'Server error' });
-        }
-    }
-
-    public async updateProductReview(
-        req: Request,
-        res: Response
-    ): Promise<void | Response> {
-        const reviewId: number = Number(req.params.id);
-        const { details } = req.body;
-
-        try {
-            const updatedReview = await this.ratingService.updateProductReview(
-                reviewId,
-                details
-            );
-            return res.status(200).json({ updatedReview });
-        } catch (error) {
-            if (error instanceof RatingNotFoundError) {
-                console.error('Error updating review: ', error);
-                return res.status(404).json({ message: error.message });
-            }
-
-            console.error('Error updating review: ', error);
-            return res.status(500).json({ message: 'Server error' });
-        }
-    }
-
-    public async deleteProductReview(
-        req: Request,
-        res: Response
-    ): Promise<void | Response> {
-        const reviewId: number = Number(req.params.id);
-        const { username } = req.body;
-
-        try {
-            await this.ratingService.deleteProductReview(reviewId);
+            await this.ratingService.deletePlatformRatingById(ratingId);
             res.sendStatus(204);
 
-            if (username) {
-                await this.adminLogsService!.log(username, 'review', 'delete');
-            }
+            await this.adminLogsService!.log(username, 'rating', 'delete');
         } catch (error) {
             if (error instanceof RatingNotFoundError) {
-                console.error('Error deleting review: ', error);
+                console.error('Error deleting rating: ', error);
                 return res.status(404).json({ message: error.message });
             }
 
-            console.error('Error deleting review: ', error);
+            console.error('Error deleting rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async deleteOwnProductRating(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const ratingId: number = Number((req.user as JwtPayload).id);
+
+        try {
+            await this.ratingService.deleteProductRatingById(ratingId);
+            res.sendStatus(204);
+        } catch (error) {
+            if (error instanceof RatingNotFoundError) {
+                console.error('Error deleting rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error deleting rating: ', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+
+    public async deleteProductRatingById(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        const ratingId: number = Number(req.params.id);
+        const { username } = req.user as JwtPayload;
+
+        try {
+            await this.ratingService.deleteProductRatingById(ratingId);
+            res.sendStatus(204);
+
+            await this.adminLogsService!.log(username, 'rating', 'delete');
+        } catch (error) {
+            if (error instanceof RatingNotFoundError) {
+                console.error('Error deleting rating: ', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error deleting rating: ', error);
             return res.status(500).json({ message: 'Server error' });
         }
     }
