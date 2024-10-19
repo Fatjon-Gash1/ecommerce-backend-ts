29a234b74aa44aeabf939aa622a9c9709fb836f1
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-19 17:05:47 +0200

    Create admins orders route

---
+++ server/routes/protected/private/orders.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { OrderController } from '../../../controllers/Order.controller';
+import { OrderService, AdminLogsService } from '../../../services';
+import { validateId, validationErrors } from '../../../middlewares/validation';
+
+const router: Router = Router();
+const orderController = new OrderController(
+    new OrderService(),
+    new AdminLogsService()
+);
+
+router.get('/', orderController.getAllOrders.bind(orderController));
+
+router.patch(
+    '/mark-as-delivered',
+    validateId(),
+    validationErrors,
+    orderController.markAsDelivered.bind(orderController)
+);
+
+export default router;
