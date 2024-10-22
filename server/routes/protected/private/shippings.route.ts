5092df100d05bea26b4235f9f1d6503a8a33fdad
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-10-23 00:09:43 +0200

    Add admin shipping route

---
+++ server/routes/protected/private/shippings.route.ts
@@ -0,0 +1,80 @@
+import { Router } from 'express';
+import { ShippingController } from '../../../controllers/Shipping.controller';
+import { ShippingService, AdminLogsService } from '../../../services';
+import { shippingUpdateRateLimiter } from '../../../middlewares/rateLimiting';
+import {
+    validateShippingCountry,
+    validateShippingCity,
+    validateShippingRate,
+    validateId,
+    validateCountryId,
+    validationErrors,
+} from '../../../middlewares/validation';
+
+const router: Router = Router();
+const shippingController = new ShippingController(
+    new ShippingService(),
+    new AdminLogsService()
+);
+
+router.post(
+    '/countries',
+    validateShippingCountry(),
+    validationErrors,
+    shippingController.addShippingCountry.bind(shippingController)
+);
+router.post(
+    '/countries/:countryId/cities',
+    validateCountryId(),
+    validateShippingCity(),
+    validationErrors,
+    shippingController.addCityToCountry.bind(shippingController)
+);
+
+router.patch(
+    '/countries/:countryId',
+    shippingUpdateRateLimiter,
+    validateCountryId(),
+    validateShippingCountry(),
+    validationErrors,
+    shippingController.updateShippingCountry.bind(shippingController)
+);
+router.patch(
+    '/countries/:countryId/cities/:id',
+    shippingUpdateRateLimiter,
+    validateCountryId(),
+    validateId(),
+    validateShippingCity(),
+    validationErrors,
+    shippingController.updateShippingCity.bind(shippingController)
+);
+router.patch(
+    '/weight-rate',
+    shippingUpdateRateLimiter,
+    validateShippingRate(),
+    validationErrors,
+    shippingController.changeShippingWeightRate.bind(shippingController)
+);
+router.patch(
+    '/method-rate',
+    shippingUpdateRateLimiter,
+    validateShippingRate(),
+    validationErrors,
+    shippingController.changeShippingMethodRate.bind(shippingController)
+);
+
+router.delete(
+    '/countries/:countryId',
+    validateCountryId(),
+    validationErrors,
+    shippingController.deleteShippingCountry.bind(shippingController)
+);
+router.delete(
+    '/countries/:countryId/cities/:id',
+    validateCountryId(),
+    validateId(),
+    validationErrors,
+    shippingController.deleteShippingCity.bind(shippingController)
+);
+
+export default router;
