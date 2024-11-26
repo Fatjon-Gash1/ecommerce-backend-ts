2a17c0003245f72e49cd02254e827cb3e5a3a644
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-11-27 00:02:51 +0100

    Create Shipping Weight model

---
+++ server/models/document/ShippingWeight.model.ts
@@ -0,0 +1,23 @@
+import { mongoose } from '../../config/mongodb';
+const { Schema, model } = mongoose;
+
+export interface IShippingWeight extends Document {
+    weight: 'light' | 'standard' | 'heavy';
+    rate: number;
+}
+
+const shippingWeightSchema = new Schema<IShippingWeight>({
+    weight: {
+        type: String,
+        enum: ['light', 'standard', 'heavy'],
+        required: true,
+    },
+    rate: { type: Number, required: true },
+});
+
+const ShippingWeight = model<IShippingWeight>(
+    'ShippingWeight',
+    shippingWeightSchema
+);
+
+export default ShippingWeight;
