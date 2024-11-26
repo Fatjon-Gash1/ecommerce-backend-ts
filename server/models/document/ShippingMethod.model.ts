01c3b11622644d678106bbb554718fc4cb0331a2
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-11-27 00:02:38 +0100

    Create Shipping Method model

---
+++ server/models/document/ShippingMethod.model.ts
@@ -0,0 +1,23 @@
+import { mongoose } from '../../config/mongodb';
+const { Schema, model } = mongoose;
+
+export interface IShippingMethod extends Document {
+    method: 'standard' | 'express' | 'next-day';
+    rate: number;
+}
+
+const shippingMethodSchema = new Schema<IShippingMethod>({
+    method: {
+        type: String,
+        enum: ['standard', 'express', 'next-day'],
+        required: true,
+    },
+    rate: { type: Number, required: true },
+});
+
+const ShippingMethod = model<IShippingMethod>(
+    'ShippingMethod',
+    shippingMethodSchema
+);
+
+export default ShippingMethod;
