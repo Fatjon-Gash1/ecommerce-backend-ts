75a403760028db3153e80aeb2dc2b214b63c3d13
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-11-27 00:33:05 +0100

    Refactor instance methods & fix minor settings

--- server/models/relational/Cart.model.ts
+++ server/models/relational/Cart.model.ts
@@ -1,6 +1,7 @@
-import { DataTypes, Model, Op } from 'sequelize';
+import { DataTypes, BelongsToManyGetAssociationsMixin, Model } from 'sequelize';
 import { sequelize } from '../../config/db';
 import { Product } from './Product.model';
+import { Customer } from './Customer.model';
 
 interface CartAttributes {
     id?: number;
@@ -8,7 +9,6 @@ interface CartAttributes {
 }
 
 interface CartItemAttributes {
-    id?: number;
     cartId?: number;
     productId?: number;
     quantity: number;
@@ -17,21 +17,16 @@ interface CartItemAttributes {
 export class Cart extends Model<CartAttributes> implements CartAttributes {
     declare id?: number;
     declare customerId?: number;
+    declare getProducts: BelongsToManyGetAssociationsMixin<Product>;
 
     public async getItems(): Promise<CartItem[]> {
-        return CartItem.findAll({ where: { cartId: this.id } });
+        return await CartItem.findAll({ where: { cartId: this.id } });
     }
 
     public async getTotalPrice(): Promise<number> {
         const cartItems: Array<CartItem> = await this.getItems();
 
-        const productIds: Array<number> = cartItems
-            .map((item) => item.productId)
-            .filter((id): id is number => id !== undefined);
-
-        const products = await Product.findAll({
-            where: { id: { [Op.in]: productIds } },
-        });
+        const products = await this.getProducts();
 
         const productPriceMap: Record<number, number> = {};
         products.forEach((product) => {
@@ -43,23 +38,37 @@ export class Cart extends Model<CartAttributes> implements CartAttributes {
             return acc + price * item.quantity;
         }, 0);
 
-        return totalPrice;
+        return totalPrice ? Math.ceil(totalPrice) - 0.01 : 0;
     }
 }
 
-Cart.init({}, { sequelize, modelName: 'Cart', tableName: 'carts' });
+Cart.init(
+    {
+        customerId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            references: { model: Customer, key: 'id' },
+        },
+    },
+    { sequelize, modelName: 'Cart', tableName: 'carts' }
+);
 
 export class CartItem
     extends Model<CartItemAttributes>
     implements CartItemAttributes
 {
-    declare id?: number;
     declare cartId?: number;
     declare productId?: number;
     declare quantity: number;
 }
 
 CartItem.init(
-    { quantity: { type: DataTypes.INTEGER, defaultValue: 0 } },
-    { sequelize, modelName: 'CartItem', tableName: 'cart_items' }
+    { quantity: { type: DataTypes.INTEGER, defaultValue: 1 } },
+    {
+        sequelize,
+        modelName: 'CartItem',
+        tableName: 'cart_items',
+        timestamps: false,
+    }
 );
