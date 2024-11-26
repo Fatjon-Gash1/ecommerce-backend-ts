841bfddb1164b1248715b2ac72955b4b535fda20
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-11-27 00:10:29 +0100

    Remove unnecessary trim

--- server/middlewares/validation/cartValidations.ts
+++ server/middlewares/validation/cartValidations.ts
@@ -2,14 +2,12 @@ import { body, ValidationChain } from 'express-validator';
 
 export const validateCartItemDetails = (): ValidationChain[] => [
     body('productId')
-        .trim()
         .notEmpty()
         .withMessage('Product is required')
         .isInt({ min: 1 })
         .withMessage('Product must be a positive number'),
 
     body('quantity')
-        .trim()
         .notEmpty()
         .withMessage('Quantity is required')
         .isInt({ min: 1 })
