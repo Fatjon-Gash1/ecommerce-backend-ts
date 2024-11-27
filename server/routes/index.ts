d3f09122cfe9e164b373ff96eaebf097fcae94ad
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-11-27 12:58:13 +0100

    Refactor route endpoints

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,27 +1,13 @@
 import { Router, Request, Response } from 'express';
-import userRoutes from './users.route';
-import productRoutes from './products.route';
-import cartRoutes from './carts.route';
-import orderRoutes from './orders.route';
-import shippingRoutes from './shipping.route';
-import paymentRoutes from './payments.route';
-import analyticRoutes from './analytics.route';
-import ratingRoutes from './ratings.route';
+import userRoutes from './protected/users.route';
+import productRoutes from './public/products.route';
+import ratingRoutes from './public/ratings.route';
+
 const router: Router = Router();
 
 router.use('/users', userRoutes);
 
 router.use('/products', productRoutes);
-
-router.use('/carts', cartRoutes);
-
-router.use('/orders', orderRoutes);
-
-router.use('/shipping', shippingRoutes);
-
-router.use('/payments', paymentRoutes);
-
-router.use('/analytics', analyticRoutes);
 
 router.use('/ratings', ratingRoutes);
 
