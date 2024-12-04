3cf9982336141770bb3c475759a31ecd1fd20752
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-12-04 19:58:10 +0100

    Add response types for methods and refactor

--- server/services/Product.service.ts
+++ server/services/Product.service.ts
@@ -26,14 +26,42 @@ interface ProductObject {
     price: number;
 }
 
+interface CategoryResponse {
+    id?: number;
+    name: string;
+    description: string;
+    hasProducts?: boolean;
+    parentId?: number | null;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+interface ProductResponse {
+    id?: number;
+    categoryId?: number;
+    name: string;
+    description: string;
+    price: number;
+    discount?: number;
+    imageUrl: string;
+    stockQuantity?: number;
+    weight: number;
+    views?: number;
+    updatedAt?: Date;
+    createdAt?: Date;
+}
+
 /**
  * Service responsible for product-related operations.
  */
 export class ProductService {
     /**
      * Creates a new category in the database.
+     *
      * @param name - The name of the category
-     * @returns The created category
+     * @param description - The description of the category
+     * @param parentId - The id of the parent category
+     * @returns A promise resolving to the created category
      *
      * @throws {@link CategoryAlreadyExistsError}
      * Thrown if the category already exists.
@@ -42,59 +70,62 @@ export class ProductService {
         name: string,
         description: string,
         parentId: number | null
-    ): Promise<Category> {
+    ): Promise<CategoryResponse> {
         const [category, created] = await Category.findOrCreate({
             where: { name },
             defaults: { name, description, parentId },
         });
 
-        if (created) {
+        if (!created) {
             throw new CategoryAlreadyExistsError();
         }
 
-        return category;
+        return category.toJSON();
     }
 
     /**
-     * Creates a new product in the database.
+     * Creates a new product in a given category.
      *
-     * @param categoryId - The ID of the category
+     * @param categoryId - The id of the category
      * @param details - The product creation details
-     * @returns The created product
-     * @throws ProductAlreadyExistsError if the product already exists
+     * @returns A promise that resolves to the created product
+     *
+     * @throws {@link CategoryNotFoundError}
+     * Thrown if the category is not found.
+     *
+     * @throws {@link ProductAlreadyExistsError}
+     * Thrown if the product already exists or if the
+     * product image is already in use.
      */
     public async addProductByCategoryId(
         categoryId: number,
         details: ProductDetails
-    ): Promise<Product> {
+    ): Promise<ProductResponse> {
         const category = await Category.findByPk(categoryId);
 
         if (!category) {
             throw new CategoryNotFoundError();
         }
 
-        const [product, created] = await Product.findOrCreate({
-            where: { name: details.name },
-            defaults: {
-                categoryId,
-                name: details.name,
-                description: details.description,
-                price: details.price,
-                discount: details.discount,
-                imageUrl: details.imageUrl,
-                stockQuantity: details.stockQuantity,
-                weight: details.weight,
-                views: details.views,
-            },
-        });
+        const [productName, productImage] = await Promise.all([
+            Product.findOne({ where: { name: details.name } }),
+            Product.findOne({ where: { imageUrl: details.imageUrl } }),
+        ]);
 
-        if (!created) {
-            throw new ProductAlreadyExistsError(
-                `Product with name: "${details.name}" already exists`
-            );
+        if (productName) {
+            throw new ProductAlreadyExistsError();
         }
 
-        return product;
+        if (productImage) {
+            throw new ProductAlreadyExistsError('Product image already in use');
+        }
+
+        const newProduct = await Product.create({
+            categoryId,
+            ...details,
+        });
+
+        return newProduct.toJSON();
     }
     /*// Product addition threshold for sending product promotions email
             let productsForPromotion: number = 0; // Will be converted to an array of Products in the future
@@ -237,59 +268,63 @@ export class ProductService {
     /**
      * Retrieves the category of a product.
      *
-     * @param productId - The ID of the product
+     * @param productId - The id of the product
      * @returns A promise resolving to a Category instance
+     *
+     * @throws {@link ProductNotFoundError}
+     * Thrown if the product is not found.
+     *
+     * @throws {@link CategoryNotFoundError}
+     * Thrown if the category is not found.
      */
-    public async getProductCategory(productId: number): Promise<Category> {
+    public async getProductCategory(
+        productId: number
+    ): Promise<CategoryResponse> {
         const product = await Product.findByPk(productId);
 
         if (!product) {
             throw new ProductNotFoundError();
         }
 
-        const category = await product.getCategory();
+        const category = await Category.findByPk(product.categoryId, {
+            attributes: { exclude: ['deletedAt'] },
+        });
 
         if (!category) {
             throw new CategoryNotFoundError();
         }
 
-        return category;
+        return category.toJSON();
     }
 
     /**
-     * Retrieves all products that are in stock.
+     * Retrieves all products by stock status.
      *
+     * @param status - The stock status ('inStock' or 'outOfStock')
      * @returns A promise resolving to an array of Product instances
      */
-    public async getProductsInStock(): Promise<
-        Product[] | { message: string }
-    > {
-        const products = await Product.findAll({
-            where: { stockQuantity: { [Op.gt]: 0 } },
+    public async getProductsByStockStatus(status: string): Promise<{
+        count: number;
+        products: ProductResponse[];
+    }> {
+        const { count, rows } = await Product.findAndCountAll({
+            where:
+                status === 'in stock'
+                    ? { stockQuantity: { [Op.gt]: 0 } }
+                    : { stockQuantity: 0 },
+            attributes: { exclude: ['deletedAt'] },
         });
 
-        return products;
+        const products = rows.map((row) => row.toJSON());
+
+        return { count, products };
     }
 
     /**
-     * Retrieves all products that are out of stock.
+     * Retrieves the product's discounted price.
      *
-     * @returns A promise resolving to an array of Product instances
-     */
-    public async getProductsOutOfStock(): Promise<
-        Product[] | { message: string }
-    > {
-        const products = await Product.findAll({
-            where: { stockQuantity: 0 },
-        });
-
-        return products;
-    }
-
-    /**
-     * Calculates the discounted price of a product.
+     * @param productId - The id of the product
      *
-     * @param productId - The ID of the product
      * @returns A promise resolving to the discounted price
      *
      * @throws {@link ProductNotFoundError}
@@ -305,7 +340,11 @@ export class ProductService {
         if (product.discount === 0) {
             return product.price;
         }
-        return product.getPriceWithDiscount();
+
+        const discountedPrice =
+            product.price - (product.price * product.discount!) / 100;
+
+        return Math.ceil(discountedPrice) - 0.01;
     }
 
     /**
@@ -323,22 +362,26 @@ export class ProductService {
         categoryId: number,
         name: string,
         description: string
-    ): Promise<Category> {
-        const category = await Category.findByPk(categoryId);
+    ): Promise<CategoryResponse> {
+        const category = await Category.findByPk(categoryId, {
+            attributes: { exclude: ['deletedAt'] },
+        });
 
         if (!category) {
             throw new CategoryNotFoundError();
         }
 
-        return await category.update({ name, description });
+        await category.update({ name, description });
+
+        return category.toJSON();
     }
 
     /**
      * Sets the discount for a product.
      *
-     * @param productId - The ID of the product
+     * @param productId - The id of the product
      * @param discount - The discount to set
-     * @returns A promise resolving to the updated product
+     * @returns A promise resolving to the discount and the new price
      */
     public async setDiscountForProduct(
         productId: number,
@@ -352,33 +395,51 @@ export class ProductService {
 
         product.discount = discount;
         await product.save();
-        return await this.getDiscountedPrice(productId);
+
+        if (product.discount === 0) {
+            return product.price;
+        }
+
+        const discountedPrice =
+            product.price - (product.price * product.discount!) / 100;
+
+        return Math.ceil(discountedPrice) - 0.01;
     }
 
     /**
      * Updates an existing product in the database.
+     *
      * @param productId - The id of the product to update
      * @param details - The product update details
-     * @returns The updated product
-     * @throws ProductNotFoundError if the product doesn't exist
+     * @returns A promise resolving to the updated product
+     *
+     * @throws {@link ProductNotFoundError}
+     * Thrown if the product doesn't exist.
      */
-    public async updateProduct(
+    public async updateProductById(
         productId: number,
         details: ProductDetails
-    ): Promise<Product> {
-        const product = await Product.findByPk(productId);
+    ): Promise<ProductResponse> {
+        const product = await Product.findByPk(productId, {
+            attributes: { exclude: ['deletedAt'] },
+        });
 
         if (!product) {
             throw new ProductNotFoundError();
         }
 
-        return await product.update(details);
+        await product.update(details);
+
+        return product.toJSON();
     }
 
     /**
-     * Deletes a category by ID.
+     * Deletes a category by id.
      *
-     * @param categoryId - The ID of the category
+     * @param categoryId - The id of the category
+     *
+     * @throws {@link CategoryNotFoundError}
+     * Thrown if the category is not found.
      */
     public async deleteCategoryById(categoryId: number): Promise<void> {
         const transaction = await sequelize.transaction();
@@ -401,9 +462,12 @@ export class ProductService {
     }
 
     /**
-     * Deletes a product by ID.
+     * Deletes a product by id.
      *
-     * @param productId - The ID of the product
+     * @param productId - The id of the product
+     *
+     * @throws {@link ProductNotFoundError}
+     * Thrown if the product is not found.
      */
     public async deleteProductById(productId: number): Promise<void> {
         const product = await Product.findByPk(productId);
