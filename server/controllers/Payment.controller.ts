0fd8a42015d1771a71537b57a1bdb9d4578bf6f1
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-12-13 16:03:47 +0100

    Add create customer method

--- server/controllers/Payment.controller.ts
+++ server/controllers/Payment.controller.ts
@@ -1,11 +1,41 @@
 import { Request, Response } from 'express';
 import { PaymentService } from '../services';
+import { UserNotFoundError } from '../errors';
 
 export class PaymentController {
     private paymentService: PaymentService;
 
     constructor(paymentService: PaymentService) {
         this.paymentService = paymentService;
+    }
+
+    public async findOrCreateCustomer(
+        req: Request,
+        res: Response
+    ): Promise<void | Response> {
+        //const customerId = Number(req.params.id);
+
+        try {
+            /*const { created, customer } =
+                await this.paymentService.findOrCreateCustomer(customerId);
+
+            return res
+                .status(200)
+                .json(
+                    created
+                        ? { newCustomer: true, customer }
+                        : { newCustomer: false, customer }
+                );*/
+            return res.sendStatus(200);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                console.error('Error finding or creating customer:', error);
+                return res.status(404).json({ message: error.message });
+            }
+
+            console.error('Error finding or creating customer:', error);
+            return res.status(500).json({ message: 'Server error' });
+        }
     }
 
     public async createPaymentIntent(
@@ -31,7 +61,7 @@ export class PaymentController {
                 console.error('Error creating payment intent:', err);
                 return res
                     .status(500)
-                    .json({ success: false, message: err.message });
+                    .json({ success: false, message: 'Server error' });
             }
         }
     }
@@ -52,7 +82,7 @@ export class PaymentController {
                 console.error('Error retrieving payment intent:', err);
                 return res
                     .status(500)
-                    .json({ success: false, message: err.message });
+                    .json({ success: false, message: 'Server error' });
             }
         }
     }
