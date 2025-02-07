770f5e1155f34395cdb0364aa783ffddfc562b6e
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-02-07 16:31:09 +0100

    Add platform data validation

--- server/middlewares/validation/adminValidations.ts
+++ server/middlewares/validation/adminValidations.ts
@@ -36,3 +36,60 @@ export const validateAttribute = (): ValidationChain[] => [
         .isLength({ max: 320 })
         .withMessage('Attribute must be no longer than 320 characters'),
 ];
+
+export const validatePlatformData = (): ValidationChain[] => [
+    body('companyName')
+        .optional()
+        .trim()
+        .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\d\s.-]+$/)
+        .withMessage(
+            'Company name must contain only letters, numbers, dots, or hyphens'
+        ),
+    body('headquartersAddress')
+        .optional()
+        .trim()
+        .matches(
+            /^[A-Za-zÀ-ÖØ-öø-ÿ'’.\-\s]+\.\s\d+,\s\d{5}\s[A-Za-zÀ-ÖØ-öø-ÿ'’\-\s]+,\s[A-Z\s]+$/
+        )
+        .withMessage(
+            'Shipping address must contain only letters, numbers, dots, commas, or hyphens'
+        ),
+
+    body('customerSupportEmail')
+        .optional()
+        .trim()
+        .isEmail()
+        .withMessage('Invalid email format')
+        .normalizeEmail(),
+
+    body('customerSupportPhoneNumber')
+        .optional()
+        .trim()
+        .matches(/^(\+?\d{1,4}[-. ]?\(?\d{1,3}\)?[-. ]?\d{1,4}[-. ]?\d{3,6})$/)
+        .withMessage('Invalid phone number format'),
+
+    body('operatingHours')
+        .optional()
+        .trim()
+        .matches(
+            /^(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM) - (0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/
+        ),
+
+    body('faq')
+        .optional()
+        .isObject()
+        .withMessage('FAQ must be an object')
+        .bail(),
+
+    body('faq.question')
+        .optional()
+        .trim()
+        .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\d\s.,'":?!()&_-]+$/)
+        .withMessage('Question must contain only valid characters'),
+
+    body('faq.answer')
+        .optional()
+        .trim()
+        .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\d\s.,'":?!()&_-]+$/)
+        .withMessage('Answer must contain only valid characters'),
+];
