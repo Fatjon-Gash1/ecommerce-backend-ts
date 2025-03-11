00e4c90fbefc27748e35371687b8993426234f2a
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-12 00:34:26 +0100

    Refactor replenishment worker logic

--- services/subscription_service/Worker.service.ts
+++ services/subscription_service/Worker.service.ts
@@ -1,115 +1,39 @@
 import { Worker } from 'bullmq';
 import IORedis from 'ioredis';
-import type { Job } from 'bullmq';
-import {
-    Product,
-    Replenishment,
-    Customer,
-    User,
-} from '../../models/relational';
-import { PlatformData } from '../../models/document';
-import { sequelize } from '../../config/db';
-import type { Transaction } from 'sequelize';
-import { ShippingService } from '../Shipping.service';
+import { sequelize } from '@/config/db';
+import { workerRedisClient } from '@/config/redis';
 import { PaymentService } from '../Payment.service';
 import { OrderService } from '../Order.service';
-import { ProductNotFoundError, UserNotFoundError } from '../../errors';
+import { ShippingService } from '../Shipping.service';
 import { NotificationService } from '../Notification.service';
-import { readFile } from 'fs/promises';
-import path from 'path';
-import * as Handlebars from 'handlebars';
+import { LoggerService } from '../Logger.service';
+import { Replenishment, Customer, User } from '@/models/relational';
+import { PlatformData } from '@/models/document';
+import { UserNotFoundError } from '@/errors';
+import type { Transaction } from 'sequelize';
+import type { Job } from 'bullmq';
 
-const TEMPLATES_PATH = process.env.TEMPLATES_PATH as string;
 const CLIENT_URL = process.env.CLIENT_URL as string;
-
-interface OrderItem {
-    productId: number;
-    quantity: number;
-}
-
-interface SuccessfulPaymentEmailData {
-    customerName: string;
-    orderTrackingNumber: string;
-    deliveryDate: string;
-    paymentAmount: string;
-    deliveryAddress: string;
-    subscriptionStateInfo: string;
-    manageSubscriptionLink: string;
-    customerSupportEmail: string;
-    customerSupportPhoneNumber: string;
-}
-
-interface FailedPaymentEmailData {
-    manageSubscriptionLink: string;
-    customerSupportEmail: string;
-    customerSupportPhoneNumber: string;
-}
-
-type WeightRange = 'light' | 'standard' | 'heavy';
-
-async function processPayment(
-    job: Job
-): Promise<{ paymentAmount: number; weightRange: WeightRange }> {
-    const customer = await Customer.findOne({
-        where: { userId: job.data.userId },
-    });
-
-    if (!customer) {
-        throw new UserNotFoundError('Customer not found');
-    }
-
-    const shippingService = new ShippingService();
-    const paymentService = new PaymentService(process.env.STRIPE_KEY as string);
-
-    const productTotal: number = await Promise.all(
-        job.data.orderItems.map(async (item: OrderItem) => {
-            const product = await Product.findByPk(item.productId, {
-                attributes: ['price'],
-            });
-
-            if (!product) {
-                throw new ProductNotFoundError();
-            }
-
-            return product.price * item.quantity;
-        })
-    ).then((prices) => prices.reduce((acc, price) => acc + price, 0));
-
-    const { cost: shippingCost, weightRange } =
-        await shippingService.calculateShippingCost(
-            job.data.shippingCountry,
-            'next-day',
-            undefined,
-            job.data.orderItems
-        );
-
-    const totalAmount: number = parseFloat(
-        (productTotal + shippingCost).toFixed(2)
-    );
-
-    await paymentService.createPaymentIntent(
-        job.data.userId,
-        totalAmount,
-        job.data.currency,
-        job.data.paymentMethodId
-    );
-
-    console.log('Payment intent created successfully!');
-
-    return { paymentAmount: totalAmount, weightRange };
-}
 
 export class WorkerService {
     private connection: IORedis;
     private queueName: string;
     private worker: Worker;
+    private paymentService: PaymentService;
     private notificationService: NotificationService;
+    private logger: LoggerService;
 
     constructor(queueName: string) {
-        this.connection = new IORedis({ maxRetriesPerRequest: null });
+        this.connection = workerRedisClient;
         this.queueName = queueName;
         this.worker = this.instantiateWorker();
+        this.paymentService = new PaymentService(
+            process.env.STRIPE_KEY as string,
+            undefined,
+            new ShippingService()
+        );
         this.notificationService = new NotificationService();
+        this.logger = new LoggerService();
     }
 
     private instantiateWorker(): Worker {
@@ -117,9 +41,12 @@ export class WorkerService {
             this.queueName,
             async (job: Job) => {
                 try {
-                    return await processPayment(job);
+                    return await this.paymentService.processPayment(
+                        job.data.userId,
+                        job.data
+                    );
                 } catch (error) {
-                    console.error(error);
+                    this.logger.error('Error from worker: ' + error);
                     throw new Error(
                         `"${this.queueName}" worker couldn't process it.`
                     );
@@ -130,29 +57,6 @@ export class WorkerService {
                 connection: this.connection,
             }
         );
-    }
-
-    public async sendReplenishmentPaymentEmail(
-        userEmail: string,
-        subject: string,
-        emailTemplate: string,
-        data: SuccessfulPaymentEmailData | FailedPaymentEmailData
-    ): Promise<void> {
-        const emailFile = await readFile(
-            path.join(TEMPLATES_PATH, emailTemplate),
-            'utf-8'
-        );
-
-        const template = Handlebars.compile(emailFile);
-
-        const htmlData = template(data);
-
-        await this.notificationService.sendEmail({
-            to: userEmail,
-            subject,
-            text: 'Fallback test',
-            html: htmlData,
-        });
     }
 
     public listen() {
@@ -202,6 +106,8 @@ export class WorkerService {
                     job.data.shippingCountry,
                     returnData.weightRange,
                     'next-day',
+                    returnData.paymentAmount,
+                    returnData.paymentIntentId,
                     transaction
                 );
 
@@ -240,8 +146,6 @@ export class WorkerService {
                     await this.connection.del(replenishment.nextJobId!);
                     replenishment.nextPaymentDate = nextPaymentDate;
 
-                    // here we call the redis method to fetch the latest scheduler job
-                    // and assign it to the nextJobId column in the db.
                     const nextJobId = (
                         await this.connection.zrange(
                             `bull:${this.queueName}:delayed`,
@@ -273,7 +177,7 @@ export class WorkerService {
 
                 const platform = (await PlatformData.find({}))[0];
 
-                await this.sendReplenishmentPaymentEmail(
+                await this.notificationService.sendReplenishmentPaymentEmail(
                     customer.user.email,
                     'Replenishment payment finished',
                     'successful-payment-email.hbs',
@@ -303,17 +207,19 @@ export class WorkerService {
                 );
             } catch (error) {
                 await transaction.rollback();
-                console.error("Error from worker's complete event: ", error);
+                this.logger.error(
+                    "Error from worker's complete event: " + error
+                );
             }
         });
 
         this.worker.on('failed', async (job, err) => {
-            console.error(
+            this.logger.error(
                 `Job${job ? ` with id "${job.id}"` : ''} has failed because ${err.message}`
             );
 
             if (!job) {
-                return console.error('Failed job not found!');
+                return this.logger.error('Failed job not found!');
             }
 
             console.log('Retry attempt: ', job.attemptsMade);
@@ -350,7 +256,7 @@ export class WorkerService {
 
                     const platform = (await PlatformData.find({}))[0];
 
-                    await this.sendReplenishmentPaymentEmail(
+                    await this.notificationService.sendReplenishmentPaymentEmail(
                         customer.user!.email,
                         'Replenishment payment failed',
                         'failed-payment-email.hbs',
@@ -363,21 +269,21 @@ export class WorkerService {
                     );
                 } catch (err) {
                     if (err instanceof UserNotFoundError) {
-                        return console.error(
+                        return this.logger.error(
                             `Email on failed replenishment payment could not be sent. Customer with userId "${job.data.userId}" was not found.`
                         );
                     }
 
-                    console.error(
-                        'Email on failed replenishment payment could not be sent. An unexpected error occurred: ',
-                        err
+                    this.logger.error(
+                        'Email on failed replenishment payment could not be sent. An unexpected error occurred: ' +
+                            err
                     );
                 }
             }
         });
 
         this.worker.on('error', (err) => {
-            console.error('Error from worker: ', err);
+            this.logger.error('Error from worker: ' + err);
         });
     }
 }
