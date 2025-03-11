6e5b8414189a2a9f9f58da3da9b0b0016b80d333
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-12 00:57:28 +0100

    Import and instantiate new config libs

--- server.ts
+++ server.ts
@@ -1,10 +1,20 @@
+import 'module-alias/register';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import { sequelize, connectToMongoDB } from './config/db';
-import indexRoutes from './routes';
+import { sequelize, connectToMongoDB, mongoose } from '@/config/db';
+import indexRoutes from '@/routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '@/swagger';
+import { redisClient } from '@/config/redis';
+import EsClient from '@/config/elasticsearch';
+import { io } from '@/config/socket';
+import { transporter } from './config/transporter';
+import { logger } from './services/Logger.service';
+import { listenToSocketEvents } from '@/socketEvents';
+import './queueWorkers';
 
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
@@ -12,14 +22,15 @@ const port: number = Number(process.env.PORT) || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 sequelize
     .sync()
     .then(() => {
-        console.log('Sequelize models synced with database');
+        logger.log('Sequelize models synced with database');
     })
     .catch((err: Error) => {
-        console.error('Error syncing Sequelize models:', err);
+        logger.error('Error syncing Sequelize models:' + err);
     });
 
 connectToMongoDB();
@@ -27,10 +38,36 @@ connectToMongoDB();
 app.use('/', indexRoutes);
 
 app.use((error: Error, _req: Request, res: Response) => {
-    console.error('Unknown error: ', error);
+    logger.error('Unknown error: ' + error);
     res.status(500).json({ message: 'Internal server error' });
 });
 
+listenToSocketEvents();
+
+function gracefulShutdown() {
+    logger.log('\nShutting down server...');
+
+    Promise.all([
+        redisClient.quit(),
+        mongoose.connection.close(),
+        sequelize.close(),
+        EsClient.close(),
+        transporter.close(),
+        new Promise((resolve) => io.close(resolve)),
+    ])
+        .then(() => {
+            logger.log('All connections closed. Exiting...');
+            process.exit(0);
+        })
+        .catch((err) => {
+            logger.error('Error during shutdown:' + err);
+            process.exit(1);
+        });
+}
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    logger.log(`Server is running on http://localhost:${port}`);
 });
