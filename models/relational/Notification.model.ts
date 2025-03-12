03c76bab68ef0f903b4cf33d039f48890a8fe8ae
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-12 01:02:07 +0100

    Create notification model

---
+++ models/relational/Notification.model.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Model } from 'sequelize';
+import type {
+    CreationOptional,
+    ForeignKey,
+    InferAttributes,
+    InferCreationAttributes,
+} from 'sequelize';
+import { sequelize } from '@/config/db';
+import { User } from './User.model';
+
+export class Notification extends Model<
+    InferAttributes<Notification>,
+    InferCreationAttributes<Notification>
+> {
+    declare id: CreationOptional<number>;
+    declare userId: ForeignKey<User['id']>;
+    declare message: string;
+    declare read: CreationOptional<boolean>;
+}
+
+Notification.init(
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        message: { type: DataTypes.STRING, allowNull: false },
+        read: { type: DataTypes.BOOLEAN, defaultValue: false },
+    },
+    { sequelize, tableName: 'notifications' }
+);
