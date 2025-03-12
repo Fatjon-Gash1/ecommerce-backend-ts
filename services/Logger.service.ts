234efb5a5d17b59613d8c0bc927680737a97e12d
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-12 01:00:48 +0100

    Add logger service

---
+++ services/Logger.service.ts
@@ -0,0 +1,31 @@
+import winston from 'winston';
+
+export class LoggerService {
+    private logger: winston.Logger;
+
+    constructor() {
+        this.logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.simple()
+                    ),
+                }),
+                new winston.transports.File({ filename: 'app.log' }),
+            ],
+        });
+    }
+
+    log(message: string) {
+        this.logger.info(message);
+    }
+
+    error(message: string) {
+        this.logger.error(message);
+    }
+}
+
+export const logger = new LoggerService();
