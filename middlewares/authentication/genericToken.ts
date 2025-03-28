f06277c0931176d03fd39667c7c7b8ebb9d089da
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-28 20:44:00 +0100

    Convert "passwordResetToken" auth middleware to "genericToken"

--- middlewares/authentication/genericToken.ts
+++ middlewares/authentication/genericToken.ts
@@ -2,44 +2,48 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { redisClient } from '@/config/redis';
 
-export default async function authenticatePasswordResetToken(
+export default async function authenticateGenericToken(
     req: Request,
     res: Response,
     next: NextFunction
 ): Promise<void | Response> {
     try {
         const authorizationHeader = req.header('Authorization');
-        const resetToken = authorizationHeader?.split(' ')[1];
+        const genericToken = authorizationHeader?.split(' ')[1];
 
-        if (!resetToken) {
+        if (!genericToken) {
             return res.status(401).json({
-                message: 'Cannot reset password. Reset token missing',
+                message: 'Cannot proceed. Token missing',
             });
         }
 
         await redisClient.zremrangebyscore(
-            'blacklistedTokens',
+            'blacklisted:tokens',
             '-inf',
             Date.now()
         );
 
         const blacklisted = await redisClient.zscore(
-            'blacklistedTokens',
-            resetToken
+            'blacklisted:tokens',
+            genericToken
         );
 
         if (blacklisted) {
-            return res
-                .status(400)
-                .json({ message: 'The token has already been used' });
+            return res.status(400).json({
+                message:
+                    'The token cannot be used anymore. Either it has been used or it was invalidated.',
+            });
         }
 
-        const decoded = jwt.verify(resetToken, process.env.GENERIC_TOKEN_KEY!);
+        const decoded = jwt.verify(
+            genericToken,
+            process.env.GENERIC_TOKEN_KEY!
+        );
 
         await redisClient.zadd(
-            'blacklistedTokens',
+            'blacklisted:tokens',
             Date.now() + 900000,
-            resetToken
+            genericToken
         );
 
         req.data = decoded;
@@ -47,7 +51,7 @@ export default async function authenticatePasswordResetToken(
     } catch (error) {
         if (error instanceof jwt.TokenExpiredError) {
             return res.status(401).json({
-                message: `Reset token expired at: ${error.expiredAt}`,
+                message: `Token expired at: ${error.expiredAt}`,
             });
         }
         if (error instanceof jwt.JsonWebTokenError) {
