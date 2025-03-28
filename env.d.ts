24d7692b2f898f47915b9666fb1c8515d276fe0f
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-03-28 20:45:52 +0100

    Remove undefined types

--- env.d.ts
+++ env.d.ts
@@ -1,25 +1,26 @@
 // src/env.d.ts
 declare namespace NodeJS {
     interface ProcessEnv {
-        PORT?: number;
-        REDIS_HOST?: string;
-        MYSQL_HOST?: string;
-        MYSQL_USER?: string;
-        MYSQL_PASSWORD?: string;
-        MYSQL_DATABASE?: string;
-        MONGODB_URI?: string;
-        ACCESS_TOKEN_KEY?: string;
-        REFRESH_TOKEN_KEY?: string;
-        ACCESS_TOKEN_EXPIRY?: string;
-        REFRESH_TOKEN_EXPIRY?: string;
-        GENERIC_TOKEN_KEY?: string;
-        GMAIL_USER?: string;
-        GMAIL_PASS?: string;
-        BASE_PATH?: string;
-        LLM_API_KEY?: string;
-        LLM_NAME?: string;
-        LLM_PROVIDER_API?: string;
-        STRIPE_KEY?: string;
-        CLIENT_URL?: string;
+        PORT: number;
+        REDIS_HOST: string;
+        MYSQL_HOST: string;
+        MYSQL_USER: string;
+        MYSQL_PASSWORD: string;
+        MYSQL_DATABASE: string;
+        MONGODB_URI: string;
+        ACCESS_TOKEN_KEY: string;
+        REFRESH_TOKEN_KEY: string;
+        ACCESS_TOKEN_EXPIRY: number;
+        REFRESH_TOKEN_EXPIRY: string;
+        GENERIC_TOKEN_KEY: string;
+        GMAIL_USER: string;
+        GMAIL_PASS: string;
+        BASE_PATH: string;
+        LLM_API_KEY: string;
+        LLM_NAME: string;
+        LLM_PROVIDER_API: string;
+        STRIPE_KEY: string;
+        CLIENT_URL: string;
+        REGISTRATION_LOYALTY_POINTS: number;
     }
 }
