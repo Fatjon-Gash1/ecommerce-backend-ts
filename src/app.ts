a32d11fca52460c464bf3cb0452beae3d0638c87
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-12-08 22:46:10 +0100

    chore(security): add helmet headers server protection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import indexRoutes from '@/routes';
@@ -9,6 +10,7 @@ import type { Request, Response } from 'express';
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
